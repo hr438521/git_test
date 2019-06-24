456286c3ecc4cee5eb72da40b74a0124c3da30ef
bts0019 <15837005426@163.com>
2019-06-24 22:04:48 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/lmd/LmdApplication.java
@@ -0,0 +1,13 @@
+package com.qfedu.lmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmdApplication.class, args);
+    }
+
+}
